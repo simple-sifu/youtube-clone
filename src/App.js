beeb85efcda8ba4b2fdd23cca6a051e2b6375230
simple-sifu <kattyntommy@gmail.com>
2020-08-06 01:55:58 -0400

    cleanup - remove invisible margin.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    // BEM class naming convention
+    <div className="app">
       <h1>Hello World</h1>
     </div>
   );
